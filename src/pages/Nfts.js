9fc415f3b6bbb2a245298a77174af5910af5a2ba
Chase Fleming <chaseefleming@gmail.com>
2022-02-08 12:35:31 -0800

    NFT data setup

--- src/pages/Nfts.js
+++ src/pages/Nfts.js
@@ -2,18 +2,29 @@ import React, {useEffect, useState} from "react"
 import Title from "../components/Title"
 import PopupLayout from "../components/PopupLayout"
 import {accountManager} from "../lib/AccountManager"
+import LoadingSpinner from "../components/LoadingSpinner"
+import {Flex} from "@chakra-ui/react"
 
 const NFTs = () => {
   const [account, setAccount] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([])
+
   useEffect(() => {
     async function getAccount() {
-      const account = await accountManager.getFavoriteAccount()
-      setAccount(account)
-      const url =
-        "https://flow-mainnet.g.alchemy.com/v2/89sw1zoybafxhqayvhjwf6se5yml2y99/getNFTs/?owner=0x9eef2e4511390ce4&offset=0&limit=5"
-      let response = await fetch(url)
-      const nfts = await response.json()
-      console.log(nfts)
+      try {
+        setLoading(true)
+        const account = await accountManager.getFavoriteAccount()
+        setAccount(account)
+        const url =
+          "https://flow-mainnet.g.alchemy.com/v2/89sw1zoybafxhqayvhjwf6se5yml2y99/getNFTs/?owner=0x9eef2e4511390ce4&offset=0&limit=5"
+        let response = await fetch(url)
+        const nftsRes = await response.json()
+        setData(nftsRes)
+        console.log(nftsRes)
+      } finally {
+        setLoading(false)
+      }
     }
     getAccount()
   }, [])
@@ -27,6 +38,19 @@ const NFTs = () => {
   return (
     <PopupLayout selectedPage='nfts'>
       <Title align='left'>NFTs for {address}</Title>
+      {loading ? (
+        <Flex direction='row' w='100%' h='100%' align='center' justify='center'>
+          <LoadingSpinner />
+        </Flex>
+      ) : (
+        <div>
+          {data.nfts.map(nft => {
+            return (
+              <div key={nft.id.tokenId}>{nft.id.tokenId}</div>
+            )
+          })}
+        </div>
+      )}
     </PopupLayout>
   )
 }
