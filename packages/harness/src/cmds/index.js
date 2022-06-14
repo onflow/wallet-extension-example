df7c04f763d8182fca8966c4220e83941fc3e138
Greg Santos <greg.santos@dapperlabs.com>
2022-06-14 15:17:24 -0700

    Update FCL harness

--- packages/harness/src/cmds/index.js
+++ packages/harness/src/cmds/index.js
@@ -4,6 +4,5 @@ import * as Q1 from "./q1"
 import * as Q2 from "./q2"
 import * as M1 from "./m1"
 import * as M2 from "./m2"
-import * as US1 from "./us1"
 
-export const COMMANDS = [LOGIN, LOGOUT, Q1, Q2, M1, M2, US1]
+export const COMMANDS = [LOGIN, LOGOUT, Q1, Q2, M1, M2]
